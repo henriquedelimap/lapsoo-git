223e8dd001860be8472761a64160f035270310f8
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-05-19 02:35:15 -0300

    stories leads scroll

--- src/pages/layout/HomeLayout/LeadsLayout/index.jsx
+++ src/pages/layout/HomeLayout/LeadsLayout/index.jsx
@@ -1,34 +1,40 @@
 import {MdManageAccounts, MdSend, MdScheduleSend} from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import { leads } from '../../../scripts/leads'
-
+import {HorizontalScrollContainer, HorizontalScrollItem } from 'react-simple-horizontal-scroller'
 export function Leads(){
     return(
         <div className="con-card">
+            <HorizontalScrollContainer >
             {leads.map(lead=>{
                 return(         
                     <div >
-                        <Link to='../chat' className='tdn' >
-                            <div className="card-box">
+                            <HorizontalScrollItem id={lead.id} >
 
-                            <div className='perfil-container'>
-                                <span className='perfil-circle'>
-                                    <img  src={lead.img}></img>
-                                </span>
-                                <span className='status-perfil'></span>
-                            </div>
+                                <Link to='../chat' className='tdn' >
+                                    <div className="card-box">
 
-                            <div className='dfrc'>
-                                <span className='status c3' />
-                                <span className='leads-name'>                        
-                                    {lead.name}
-                                </span>
-                            </div>
-                            </div>
-                        </Link>
+                                    <div className='perfil-container'>
+                                        <span className='perfil-circle'>
+                                            <img  src={lead.img}></img>
+                                        </span>
+                                        <span className='status-perfil'></span>
+                                    </div>
+
+                                    <div className='dfrc'>
+                                        <span className='status c3' />
+                                        <span className='leads-name'>                        
+                                            {lead.name}
+                                        </span>
+                                    </div>
+                                    </div>
+                                </Link>
+
+                            </HorizontalScrollItem>
                     </div>
                 )
             })}
+            </HorizontalScrollContainer>
         </div>
     )
 }
